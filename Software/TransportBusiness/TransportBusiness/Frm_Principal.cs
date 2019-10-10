f9534dd3aca3ddfd8e7d25cfe3e5704fdc1667a3
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2019-10-10 10:31:27 -0500

    Inicio

---
+++ Software/TransportBusiness/TransportBusiness/Frm_Principal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TransportBusiness
+{
+    public partial class Frm_Principal : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public Frm_Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void barButtonItem9_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void barButtonItem11_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+    }
+}
