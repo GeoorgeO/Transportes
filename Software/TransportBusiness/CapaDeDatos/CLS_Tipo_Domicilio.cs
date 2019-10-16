e56068da4b8319d991914096774fdf73e188688c
George O <geoorge191@hotmail.com>
2019-10-16 11:55:33 -0500

    Interfaz Tipo domicilio

---
+++ Software/TransportBusiness/CapaDeDatos/CLS_Tipo_Domicilio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDeDatos
+{
+    class CLS_Tipo_Domicilio
+    {
+    }
+}
