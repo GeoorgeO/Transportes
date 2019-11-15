a3e80a375b341d9c3ea0de0d1736a113f74b0a7c
Juan Alvaro Esquivel Aguilar <iscjaea@gmail.com>
2019-11-15 13:38:46 -0600

    Cambios

---
+++ Software/TransportBusiness/TransportBusiness/Formularios/Catalogos/Frm_View_PDF.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using System.IO;
+
+namespace TransportBusiness.Formularios.Catalogos
+{
+    public partial class Frm_View_PDF : DevExpress.XtraEditors.XtraForm
+    {
+        public Frm_View_PDF()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_View_PDF_Shown(object sender, EventArgs e)
+        {
+            FileStream stream = new FileStream("..\\..\\Demo.pdf", FileMode.Open);
+            pdfViewer1.LoadDocument(stream);
+        }
+    }
+}
---
+++ Software/TransportBusiness/TransportBusiness/Formularios/Catalogos/Frm_View_XML.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace TransportBusiness.Formularios.Catalogos
+{
+    public partial class Frm_View_XML : DevExpress.XtraEditors.XtraForm
+    {
+        public Frm_View_XML()
+        {
+            InitializeComponent();
+        }
+    }
+}
